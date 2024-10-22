369bd4fd4bc6c2331e5793e64a536679cbc70eb6
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-10-21 21:15:30 -0400

    feat: scaffold server and protobuffers

---
+++ server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	reflection.Register(server)
+
+	registerServices(server)
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ server/register-services.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/eco-heroes/server/services"
+	"google.golang.org/grpc"
+)
+
+var registrableServices []RegistrableService = []RegistrableService{
+	&services.GreeterService{},
+	&services.PingService{},
+}
+
+/*
+Register all listed services into a GRPC Server
+*/
+func registerServices(server *grpc.Server) {
+	for _, s := range registrableServices {
+		s.Register(server)
+	}
+}
